8568731016c06b9c97ba0b051162cefb7f0d4521
sy do <sydo125478@gmail.com>
2016-07-14 16:08:59 +0700

    c
    c

--- index.php
+++ index.php
@@ -61,9 +61,7 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
                 $html = file_get_contents("http://tiepcankhachhang.com/fid/?uid=".$command);
                 if($html != "")
                 {
-                    $phone = explode("|", $html);
-                    $phone = $phone[0];
-                    $phone = substr($phone, 0, -3).'xxx';
+                    $phone = substr($html, 0, -3).'xxx';
                     $bot->send(new Message($message['sender']['id'], $phone));
                 }
                 else
