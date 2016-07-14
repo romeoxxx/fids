292bc17200ea9a536b70e66efe35fb7d204e74bc
sy do <sydo125478@gmail.com>
2016-07-14 15:37:30 +0700

    sad
    sad

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'verify_token' => 'fid.vn',
+    'token' => 'EAAIPsfXgLCwBAAdt1dmmB3SIvYuPDEVIZC8uL0ZAlGa4cF9Wqxlun75CZCRI4cbZC6nufZCPh5j2xOaFZAF1xTNoMYiO40DJx42j1ViBeVD122Esp81DfCFkfpnJt4tPu0ToEZBCquoZCkVWrfim3i8HPoAihLCQnhU1xZCgJPrxLpQZDZD'
+];
