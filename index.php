9dd8bddd9540fe17de758f5d7e9158441058d20e
sy do <sydo125478@gmail.com>
2016-07-19 20:39:00 +0700

    cacsac
    acsacsacsac

--- index.php
+++ index.php
@@ -65,8 +65,12 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
                             'elements' => [
                                  new MessageElement("First item", "Item description", "", [
                                     new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
-                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button2'),
-                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button1'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
                                     new MessageButton(MessageButton::TYPE_WEB, 'Web link', 'http://facebook.com')
                                 ]),
                             ]
