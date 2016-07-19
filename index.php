75602587bfe8fbc1dd3dd0c46945b059477c59c6
sy do <sydo125478@gmail.com>
2016-07-19 15:55:18 +0700

    s
    s

--- index.php
+++ index.php
@@ -58,36 +58,25 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
 
 
             if($command == '/help'){
-                $msg = '{"attachment": {
-                                "type": "template",
-                                "payload": {
-                                    "template_type": "generic",
-                                    "elements": [{
-                                        "title": "First card",
-                                        "subtitle": "Element #1 of an hscroll",
-                                        "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                                        "buttons": [{
-                                            "type": "web_url",
-                                            "url": "https://www.messenger.com",
-                                            "title": "web url"
-                                        }, {
-                                            "type": "postback",
-                                            "title": "Postback",
-                                            "payload": "Payload for first element in a generic bubble",
-                                        }],
-                                    }, {
-                                        "title": "Second card",
-                                        "subtitle": "Element #2 of an hscroll",
-                                        "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                                        "buttons": [{
-                                            "type": "postback",
-                                            "title": "Postback",
-                                            "payload": "Payload for second element in a generic bubble",
-                                        }],
-                                    }]
-                                }
-                            }';
-                $bot->send(new Message($message['sender']['id'], $msg));
+                $bot->send(new StructuredMessage($message['sender']['id'],
+                        StructuredMessage::TYPE_GENERIC,
+                        [
+                            'elements' => [
+                                new MessageElement("First item", "Item description", "", [
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_WEB, 'Web link', 'http://facebook.com')
+                                ]),
+                                new MessageElement("Second item", "Item description", "", [
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'Second button')
+                                ]),
+                                new MessageElement("Third item", "Item description", "", [
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'Second button')
+                                ])
+                            ]
+                        ]
+                    ));
             }
 
 
