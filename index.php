abaa06ea09c5fce2c29b1821c9ef2d8175c215b6
sy do <sydo125478@gmail.com>
2016-07-19 23:52:16 +0700

    các
    cá

--- index.php
+++ index.php
@@ -80,26 +80,37 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
             }
             if($command == '/his'){
                 $html = file_get_contents('http://'.'tiepcan'.'khachhang'.'.com/fid/his.php?fid='.$message['sender']['id']); 
-                $bot->send(new Message($message['sender']['id'], $html));
-            }
-            if($command == '/key'){
-                $bot->send(new Message($message['sender']['id'], 'Mã đăng ký: '.$message['sender']['id']));
-                $bot->send(new StructuredMessage($message['sender']['id'],
-                        StructuredMessage::TYPE_GENERIC,
-                        [
+                if($html != "Không tìm thấy."){
+                    $ids = explode("\n", $html);
+                    foreach ($ids as &$id) {
+                        $uid = explode(':', $id)
+
+                    }
+                    $msg = [
                             'elements' => [
                                 new MessageElement("First item", "Item description", "", [
-                                   
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_WEB, 'Web link', 'http://facebook.com')
                                 ]),
                                 new MessageElement("Second item", "Item description", "", [
-                                    
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'Second button')
                                 ]),
                                 new MessageElement("Third item", "Item description", "", [
-                                    
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'Second button')
                                 ])
                             ]
                         ]
+                    $bot->send(new StructuredMessage($message['sender']['id'],
+                        StructuredMessage::TYPE_GENERIC, $msg
+                        
                     ));
+                }
+                
+            }
+            if($command == '/key'){
+                $bot->send(new Message($message['sender']['id'], 'Mã đăng ký: '.$message['sender']['id']));
             }
 
             if($command == '/lic' && $command != '/lic '){
@@ -153,7 +164,7 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
                         StructuredMessage::TYPE_GENERIC,
                         [
                             'elements' => [
-                                 new MessageElement($html, "", "https://graph.facebook.com/".$command."/picture?width=300&height=300
+                                 new MessageElement($html, 'UID: '.$command, "https://graph.facebook.com/".$command."/picture?width=300&height=300
 ", null),
                             ]
                         ]
@@ -166,7 +177,7 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
                         StructuredMessage::TYPE_GENERIC,
                         [
                             'elements' => [
-                                 new MessageElement("Không tìm thấy", "", "https://graph.facebook.com/".$command."/picture?width=300&height=300
+                                 new MessageElement("Không tìm thấy", 'UID: '.$command, "https://graph.facebook.com/".$command."/picture?width=300&height=300
 ", null),
                             ]
                         ]
