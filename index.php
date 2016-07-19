444cd0aad56964a81e00d5e6cdd0bf71b0c79700
sy do <sydo125478@gmail.com>
2016-07-19 20:15:30 +0700

    s
    cascsac

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
-
-
 $verify_token = ""; // Verify token
 $token = ""; // Page token
-
 if (file_exists(__DIR__.'/config.php')) {
     $config = include __DIR__.'/config.php';
     $verify_token = $config['verify_token'];
     $token = $config['token'];
 }
-
 require_once(dirname(__FILE__) . '/vendor/autoload.php');
-
 use pimax\FbBotApp;
 use pimax\Messages\Message;
 use pimax\Messages\ImageMessage;
@@ -23,113 +18,134 @@ use pimax\Messages\MessageReceiptElement;
 use pimax\Messages\Address;
 use pimax\Messages\Summary;
 use pimax\Messages\Adjustment;
-
 // Make Bot Instance
 $bot = new FbBotApp($token);
-
 // Receive something
 if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) {
-
     // Webhook setup request
     echo $_REQUEST['hub_challenge'];
 } else {
-
     // Other event
-
     $data = json_decode(file_get_contents("php://input"), true, 512, JSON_BIGINT_AS_STRING);
     if (!empty($data['entry'][0]['messaging'])) {
         foreach ($data['entry'][0]['messaging'] as $message) {
-
             // Skipping delivery messages
             if (!empty($message['delivery'])) {
                 continue;
             }
-
             $command = "";
-
             // When bot receive message from user
             if (!empty($message['message'])) {
                 $command = $message['message']['text'];
-
             // When bot receive button click from user
             } else if (!empty($message['postback'])) {
                 $command = $message['postback']['payload'];
             }
-
-            if($command == '/help'){
-                $bot->send(new StructuredMessage($message['sender']['id'],
+            // Handle command
+            switch ($command) {
+                // When bot receive "text"
+                case 'text':
+                    $bot->send(new Message($message['sender']['id'], 'This is a simple text message.'));
+                    break;
+                // When bot receive "image"
+                case 'image':
+                    $bot->send(new ImageMessage($message['sender']['id'], 'https://developers.facebook.com/images/devsite/fb4d_logo-2x.png'));
+                    break;
+                // When bot receive "profile"
+                case 'profile':
+                    $user = $bot->userProfile($message['sender']['id']);
+                    $bot->send(new StructuredMessage($message['sender']['id'],
                         StructuredMessage::TYPE_GENERIC,
                         [
                             'elements' => [
-                                new MessageElement("Hướng dẫn", "Hướng dẫn sử dụng Fid.vn", "", [
-                                    new MessageButton(MessageButton::TYPE_POSTBACK, "Mã đăng ký", "USER_DEFINED_PAYLOAD")
+                                new MessageElement($user->getFirstName()." ".$user->getLastName(), " ", $user->getPicture())
+                            ]
+                        ]
+                    ));
+                    break;
+                // When bot receive "button"
+                case 'button':
+                  $bot->send(new StructuredMessage($message['sender']['id'],
+                      StructuredMessage::TYPE_BUTTON,
+                      [
+                          'text' => 'Choose category',
+                          'buttons' => [
+                              new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                              new MessageButton(MessageButton::TYPE_POSTBACK, 'Second button'),
+                              new MessageButton(MessageButton::TYPE_POSTBACK, 'Third button')
+                          ]
+                      ]
+                  ));
+                break;
+                // When bot receive "generic"
+                case 'generic':
+                    $bot->send(new StructuredMessage($message['sender']['id'],
+                        StructuredMessage::TYPE_GENERIC,
+                        [
+                            'elements' => [
+                                new MessageElement("First item", "Item description", "", [
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_WEB, 'Web link', 'http://facebook.com')
+                                ]),
+                                new MessageElement("Second item", "Item description", "", [
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'Second button')
+                                ]),
+                                new MessageElement("Third item", "Item description", "", [
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
+                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'Second button')
                                 ])
                             ]
                         ]
                     ));
-            }
-
-
-            if($command == '/his'){
-                $html = file_get_contents('http://'.'tiepcan'.'khachhang'.'.com/fid/his.php?fid='.$message['sender']['id']); 
-                $bot->send(new Message($message['sender']['id'], $html));
-            }
-            if($command == '/key'){
-                $bot->send(new Message($message['sender']['id'], 'Mã đăng ký: '.$message['sender']['id']));
-            }
-
-            if($command == '/lic' && $command != '/lic '){
-                $html = file_get_contents('http://'.'tiepcan'.'khachhang'.'.com/fid/lic.php?fid='.$message['sender']['id']); 
-                $bot->send(new Message($message['sender']['id'], $html));
-            }
-
-            if (strpos($command, '/dk ') !== false) {
-                if($message['sender']['id'] == '1196611687050818' || $message['sender']['id'] == '1099546280107865'){
-                    $val = explode(" ", $command);
-                    $url = 'http://'.'tiepcan'.'khachhang'.'.com/fid/add.php?fid='.$val[1].'&balance='.$val[2].'&memo='.urlencode($command);
-                    $html = file_get_contents($url); 
-                    $bot->send(new Message($message['sender']['id'], $html));
-                }
-                else
-                {
-                    $bot->send(new Message($message['sender']['id'], 'Bạn không có quyền sử dụng tính năng này.'));
-                }
-                
-            }
-
-
-            if (strpos($command, '/lic ') !== false) {
-                if($message['sender']['id'] == '1196611687050818' || $message['sender']['id'] == '1099546280107865'){
-                    $val = explode(" ", $command);
-                    $html = file_get_contents('http://'.'tiepcan'.'khachhang'.'.com/fid/lic.php?fid='.$val[1]); 
-                    $bot->send(new Message($message['sender']['id'], $html));
-                }
-                else
-                {
-                    $bot->send(new Message($message['sender']['id'], 'Bạn không có quyền sử dụng tính năng này.'));
-                }
-                
-            }
-
-
-
-            if(is_numeric($command))
-            {          	
-                if(strlen($command) < 9)
-            	{
-            		//$bot->send(new Message($message['sender']['id'], $command.' có thể không phải là UID'));	
-            	}
-            	else
-            	{
-	                $bot->send(new Message($message['sender']['id'], 'Đang kiểm tra UID: '.$command));
-	                $html = file_get_contents('http://'.'tiepcan'.'khachhang'.'.com/fid/?uid='.$command.'&fid='.$message['sender']['id']);
-	                if($html != "")
-	                {
-	                    	$bot->send(new Message($message['sender']['id'], $html));
-	                }
-	                else
-	                    	$bot->send(new Message($message['sender']['id'], "Không tìm thấy" ));
-            	}
+                    
+                break;
+                // When bot receive "receipt"
+                case 'receipt':
+                    $bot->send(new StructuredMessage($message['sender']['id'],
+                        StructuredMessage::TYPE_RECEIPT,
+                        [
+                            'recipient_name' => 'Fox Brown',
+                            'order_number' => rand(10000, 99999),
+                            'currency' => 'USD',
+                            'payment_method' => 'VISA',
+                            'order_url' => 'http://facebook.com',
+                            'timestamp' => time(),
+                            'elements' => [
+                                new MessageReceiptElement("First item", "Item description", "", 1, 300, "USD"),
+                                new MessageReceiptElement("Second item", "Item description", "", 2, 200, "USD"),
+                                new MessageReceiptElement("Third item", "Item description", "", 3, 1800, "USD"),
+                            ],
+                            'address' => new Address([
+                                'country' => 'US',
+                                'state' => 'CA',
+                                'postal_code' => 94025,
+                                'city' => 'Menlo Park',
+                                'street_1' => '1 Hacker Way',
+                                'street_2' => ''
+                            ]),
+                            'summary' => new Summary([
+                                'subtotal' => 2300,
+                                'shipping_cost' => 150,
+                                'total_tax' => 50,
+                                'total_cost' => 2500,
+                            ]),
+                            'adjustments' => [
+                                new Adjustment([
+                                    'name' => 'New Customer Discount',
+                                    'amount' => 20
+                                ]),
+                                new Adjustment([
+                                    'name' => '$10 Off Coupon',
+                                    'amount' => 10
+                                ])
+                            ]
+                        ]
+                    ));
+                break;
+                // Other message received
+                default:
+                    $bot->send(new Message($message['sender']['id'], 'Sorry. I don’t understand you.'));
             }
         }
     }
