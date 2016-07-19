c44c1700d896b2d305415342cf47be28955c057c
sy do <sydo125478@gmail.com>
2016-07-20 00:23:06 +0700

    cs
    c

--- index.php
+++ index.php
@@ -84,11 +84,24 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
                 $bot->send(new StructuredMessage($message['sender']['id'],
                         StructuredMessage::TYPE_GENERIC,
                         [
-                            'elements' => [
-                                new MessageElement("Second item", "Item description", "", [
-                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
-                                    new MessageButton(MessageButton::TYPE_POSTBACK, 'Second button')
-                                ])
+                            "elements":[
+                              {
+                                "title":"Welcome to Peter\'s Hats",
+                                "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
+                                "subtitle":"We\'ve got the right hat for everyone.",
+                                "buttons":[
+                                  {
+                                    "type":"web_url",
+                                    "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
+                                    "title":"View Website"
+                                  },
+                                  {
+                                    "type":"postback",
+                                    "title":"Start Chatting",
+                                    "payload":"USER_DEFINED_PAYLOAD"
+                                  }              
+                                ]
+                              }
                             ]
                         ]
                     ));
