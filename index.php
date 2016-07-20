c1ec06a8743ee17f987705978d91c3f2ca52c980
sy do <sydo125478@gmail.com>
2016-07-20 13:12:07 +0700

    áhd
    jkasdh

--- index.php
+++ index.php
@@ -85,7 +85,6 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
                     $ids = explode("\n", $html);
                     foreach ($ids as &$id) {
                         $uid = explode(":", $id);
-                        array_push($msg, new MessageElement($uid[0], $uid[1], "", null));
                     }
                     $bot->send(new StructuredMessage($message['sender']['id'],
                         StructuredMessage::TYPE_GENERIC,
