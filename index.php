19df7388d60591a83e8b95d228612a475938db8f
sy do <sydo125478@gmail.com>
2016-07-20 12:46:03 +0700

    jsh
    jskhd

--- index.php
+++ index.php
@@ -81,11 +81,11 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
             if($command == '/his'){
                 $html = file_get_contents('http://'.'tiepcan'.'khachhang'.'.com/fid/his.php?fid='.$message['sender']['id']); 
                 //$bot->send(new Message($message['sender']['id'], $html));
-                $msg = json_encode(array(array("elements" => array(new MessageElement("First item", "Item description", "",null)))));
+                $msg = json_encode(array(new MessageElement("First item", "Item description", "", null)));
                 $bot->send(new StructuredMessage($message['sender']['id'],
                         StructuredMessage::TYPE_GENERIC,
                         [
-                            $msg
+                            'elements' => $msg
                         ]
                     ));
 
