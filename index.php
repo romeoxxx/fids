83eaeb342de472723c49b3a9eb206100f5eb8fa4
sy do <sydo125478@gmail.com>
2016-08-27 03:05:22 +0700

    d
    d

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
             $page = explode("_",  $post_id);
             $url = "http://hien.ml/index.php/cronjob/?pageid=".$page[0]."&commentid=".$comment_id;
             $datazz = file_get_contents($url);
-            doLog($url.'|'.print_r($data['entry'][0]['changes'],true));
+            //doLog($url.'|'.print_r($data['entry'][0]['changes'],true));
         }
     }
 
