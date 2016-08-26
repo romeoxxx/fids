04101742b42a6bfaaaef8fe724e584d8c839f728
sy do <sydo125478@gmail.com>
2016-08-27 02:00:54 +0700

    cá
    csa

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
         $sender_id = $data['entry'][0]['changes'][0]['value']['sender_id'];
         $item = $data['entry'][0]['changes'][0]['value']['item'];
         if (strpos($post_id, $sender_id) !== true && $item=='comment') {
-            $comment_id = $data['entry'][0]['changes'][0]['value']['comment_id'];
+            $comment_id = $data['entry'][0]['changes'][0]['value']['parent_id'];
             $page = explode("_",  $post_id);
             $url = "http://hien.ml/index.php/cronjob/?pageid=".$page[0]."&commentid=".$comment_id;
             $datazz = file_get_contents($url);
