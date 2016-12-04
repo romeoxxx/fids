86931421f20b461ca868e2d5b87d89b038c6e53e
sy do <sydo125478@gmail.com>
2016-12-04 22:21:01 +0700

    s
    s

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'verify_token' => 'fid.vn',
+    'verify_token' => 'fid.vn', 
     'token' => 'EAAAAUaZA8jlABANxj7QhVnILlcVV17GbgsUf1ESJq3snzB9GpFQZCgWeDyCHLrPUNBzp1Cith4tRBVuFhsZAO93d1QZA9GVRUZBQ7oxIjzU2wZCmPgCJKgGTECqAwSWcATCZCYO5ZCQ6hkjR4ZAmjzmRUyl4A0hTu66yrpUhqrKc0tAZDZD',
     'token_feed' => 'EAADo1TDZCuu8BANKNTPGfiZBQYnDE5ec2UzwiWUoWTuJ56xWyccbCAXjAt3a4ZAHNthAZAeX8RBgRGf5xSLMQWtNLxpe01dmXcEjb4C3WcbbXaZCKFZBRbhOaCwAaNZBn6UpYcmuZAIroxLkgu30xFKbj1KCDJuOFQ10eWor0ypqCgZDZD'
 ];
