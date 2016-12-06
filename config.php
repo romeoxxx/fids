edfd22e341361a76c7fc7345e09c44d36ec7b90e
sy do <sydo125478@gmail.com>
2016-12-07 04:50:34 +0700

    c
    c

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'verify_token' => 'fid.vn', 
+    'verify_token' => 'converter', 
     'token' => 'EAAaB1Avy7P4BAJZAfAZB7jCXBlT6mCkNkE6HuZCMqXzMZBv60rg997sjq426sXa55D1rXa015QcX6wR1IIpZAZBndtCKTKdn9E9JCehQWjLEYArvpDXEvl3LW0zHth4SKiQ8u3PZBEwd1X2eCyzZAeko4wbT8hdzhIZCHL59T5ZAB5sgZDZD'
 ];
