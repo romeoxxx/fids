f572bde59f0d27eb49e3207a04d7314e4946aa9a
sy do <sydo125478@gmail.com>
2016-12-08 11:06:04 +0700

    cscscs
    cssc

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 
 return [
     'verify_token' => 'converter',
-    'token' => 'EAAaB1Avy7P4BAJZAfAZB7jCXBlT6mCkNkE6HuZCMqXzMZBv60rg997sjq426sXa55D1rXa015QcX6wR1IIpZAZBndtCKTKdn9E9JCehQWjLEYArvpDXEvl3LW0zHth4SKiQ8u3PZBEwd1X2eCyzZAeko4wbT8hdzhIZCHL59T5ZAB5sgZDZD'
+    'token' => 'EAAaB1Avy7P4BAJfZCkgVjNf8g1dyV68CnISWqNDwvHyefKao3islDgjLj5VCiwOKzXRbxF35oFplG1xIdxrYuEvsYXx5vmjG5uqdVz6k9PMUqtxIpBZCfZALWGVXlOvY2wjnkcEJBLYbCea3UIw4rTI6vD5vwUMxjprOykLtwZDZD'
 ];
